2e5f31b72c0965d0e0c1582f475e5a2d4373a6ca
James Prentice <jamesprenticez@gmail.com>
2023-03-30 19:05:36 +1300

    remove dist

--- dist/esm/types/components/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import { type ReactNode } from "react";
-interface ButtonProps {
-    label: string;
-    children?: ReactNode;
-}
-declare const Button: ({ children, label }: ButtonProps) => JSX.Element;
-export default Button;
--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-interface ButtonProps {
-    label: string;
-    children?: ReactNode;
-}
-declare const Button: ({ children, label }: ButtonProps) => JSX.Element;
-
-export { Button };
